655d5b3765ca3539c6673b1bc4347eebd9f6d967
Gynnx <37513016+pydjaproject@users.noreply.github.com>
2018-06-10 03:22:21 +0800

    added object-method samples

--- public/scripts/objects/objects-reference.js
+++ public/scripts/objects/objects-reference.js
@@ -1,14 +1,43 @@
 let myAccount = {
     name: 'Cyngynn',
-    expenses: 1000,
-    income: 10000
+    expenses: 0,
+    income: 0
 }
 
 let addExpense = (account,amount) => {
   account.expenses = account.expenses + amount;
-  console.log(account);
+  //console.log(account);
   
 };
 
+let addIncome = function (account, amount){
+  account.income = account.income + amount;
+};
+
+let resetAccount = (account) => {
+  account.income = 0;
+  account.expenses = 0;
+};
+
+
+let getAccountSummary = (account) => {
+  let balance = account.income - account.expenses;
+return `Account for ${account.name} has $${balance} income and $${account.expenses} expenses`;
+};
+
 addExpense(myAccount,500);
-console.log(addExpense);
+addIncome(myAccount,5000);
+addExpense(myAccount,500);
+addExpense(myAccount,500);
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
+
+
+
+
+
+
+
+
+
