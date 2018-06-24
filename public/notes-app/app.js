11392e1f649be06de2399bf7663f9631bd651b84
Gynnx <37513016+pydjaproject@users.noreply.github.com>
2018-06-25 02:36:42 +0800

    additional notes

---
+++ public/notes-app/app.js
@@ -0,0 +1,68 @@
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+const filters = {
+    searchText: ''
+};
+
+const renderNotes = (notes, filters) => {
+    const filteredNotes = notes.filter((note)=>{
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+
+    document.querySelector('#note').innerHTML = '';
+    
+    filteredNotes.forEach((note)=> {
+        const noteEl = document.createElement('p');
+        noteEl.textContent = note.title;
+        document.querySelector('#note').appendChild(noteEl);
+        
+    });
+    
+};
+
+renderNotes(notes,filters);
+
+// DOM - Document Object Model
+
+// Query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+// Query all and remove
+// const ps = document.querySelectorAll('p')
+
+// ps.forEach(function (p) {
+//     p.textContent = '******'
+//     // console.log(p.textContent)
+//     // p.remove()
+// })
+
+// // Add a new element
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'This is a new element from JavaScript'
+// document.querySelector('body').appendChild(newParagraph)
+
+document.querySelector('#create-note').addEventListener('click',(e) => {
+    console.log('Create Note');
+    
+});
+document.querySelector('#remove-note').addEventListener('click', (e) => {
+        console.log('Remove note');
+        document.querySelectorAll('.note').forEach((note)=>note.remove());
+    });
+
+document.querySelector('#note-input').addEventListener('input',(e)=>{
+    // console.log(e.target.value);
+    filters.searchText = e.target.value;
+    renderNotes(notes,filters);
+    
+});
